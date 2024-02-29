a9a890a7db40ce461ea1dbb55749403c9bbde14b
Javier Jimenez Fernandez <javjimen@student.42madrid.com>
2024-02-29 12:30:27 +0100

    Add missing functions to header file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,18 +6,26 @@
 /*   By: javjimen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 01:27:49 by javjimen          #+#    #+#             */
-/*   Updated: 2024/02/22 14:11:12 by javjimen         ###   ########.fr       */
+/*   Updated: 2024/02/29 12:36:44 by javjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "ft_printf.h"
 
+/* push_swap magic numbers */
 #define	PS_OK		0
 #define PS_ERROR	1
 
+/* typedef */
 typedef	int	ps_error_t;
 
+/* push swap */
 int	main(int argc, char **argv);
 
+/* input control */
+ps_error_t	is_bigger_than_int(char *str, int len);
+ps_error_t	is_duplicated(char *str1, char *str2);
 ps_error_t	input_control(int argc, char **argv);
+
+/* parse input */
